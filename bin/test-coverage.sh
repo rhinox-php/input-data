be6d9b4d17420a483139842658d7541b2b849ed3
David <d.c.neilsen@gmail.com>
2021-11-25 10:16:48 +1300

    Update test coverage script

--- bin/test-coverage.sh
+++ bin/test-coverage.sh
@@ -1,5 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DATE=`date +%Y-%m-%d_%H%M.%S`
 pushd $DIR/../
-vendor/bin/phpunit --coverage-html .test-output/coverage $@
+# @todo use paratest
+XDEBUG_MODE=coverage vendor/bin/phpunit --order-by=random --coverage-xml .test-output/run_$DATE/coverage-xml --coverage-html .test-output/run_$DATE/coverage --log-junit .test-output/run_$DATE/report.xml -d memory_limit=-1 $@
+vendor/bin/psalm --threads=$(getconf _NPROCESSORS_ONLN) --output-format=xml > .test-output/run_$DATE/psalm.xml
+vendor/bin/phpmd classes xml phpmd.xml > .test-output/run_$DATE/phpmd.xml
+rm -rf .test-output/current
+cp -rp .test-output/run_$DATE .test-output/current
 popd
